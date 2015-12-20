d7dece7a38b67a69456026a937e911f055c6d15c
Jeffrey Horn <jeffh@generalassemb.ly>
2015-12-19 21:04:51 -0500

    Create checkout modal

--- app/routes/books.js
+++ app/routes/books.js
@@ -3,5 +3,25 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('book');
+  },
+
+  actions: {
+    openCheckoutModal(book) {
+      this.controllerFor('application').set('showingModal', true);
+      return this.render('modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: this.store.findRecord('book', book.id, { reload: true }),
+        controller: 'application'
+      });
+    },
+
+    closeCheckoutModal() {
+      this.controllerFor('application').set('showingModal', false);
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
   }
 });
---
+++ tests/integration/components/checkout-modal-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('checkout-modal', 'Integration | Component | checkout modal', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{checkout-modal}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#checkout-modal}}
+      template block text
+    {{/checkout-modal}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
